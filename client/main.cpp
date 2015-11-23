7c37dd51eefdffe55165ec4bf6449eb15cf8eaf9
DecayMehls <ymschaedle@gmail.com>
2015-11-23 16:54:28 +0100

    Add Basic QT-Client

---
+++ client/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include <client.h>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Client client;
+    client.start("127.0.0.1", 1234);
+    return a.exec();
+}
+
