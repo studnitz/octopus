46553613881413756d648e36bb4ed3e3ff3ecad9
Bartosz Milejski <mil_bar@hotmail.de>
2015-11-30 13:35:14 +0100

    Add first GUI

---
+++ server_wGUI/QT-Server_wGUI/QT-Server_wGUI.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-11-30T12:18:17
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = QT-Server_wGUI
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ server_wGUI/QT-Server_wGUI/mainwindow.cpp
@@ -0,0 +1,36 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_recordButton_clicked()
+{
+    static bool startStopFlag = false;
+    if (startStopFlag == false) {
+        ui->recordButton->setText("Stop");
+        recordStart();
+        startStopFlag = true;
+    } else {
+        ui->recordButton->setText("Aufnehmen");
+        recordStop();
+        startStopFlag = false;
+    }
+}
+
+void MainWindow::recordStart() {
+    ui->debugTextEdit->insertPlainText("Aufnahme wurde gestartet\n");
+}
+
+void MainWindow::recordStop() {
+    ui->debugTextEdit->insertPlainText("Aufnahme wurde gestoppt\n");
+}
