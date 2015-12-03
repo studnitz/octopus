5d2d7057adae0ca32d9ff2bf4fa7fea019441e5f
Bartosz Milejski <mil_bar@hotmail.de>
2015-12-03 18:47:18 +0100

    Add red LED support

--- server_wGUI/QT-Server_wGUI/mainwindow.cpp
+++ server_wGUI/QT-Server_wGUI/mainwindow.cpp
@@ -2,12 +2,26 @@
 #include "ui_mainwindow.h"
 
 #include <QStandardItemModel>
+#include <QList>
+#include <QMessageBox>
+#include <stdio.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Initialisierung der Tabelle
+    ui->tableWidget->setColumnCount(2);
+    ui->tableWidget->setColumnWidth(0,160);
+    ui->tableWidget->setColumnWidth(1,20);
+    ui->tableWidget->setShowGrid(false);
+
+    // Initialisierung der Clients-Liste
+    clients = new QList<QString>();
+    clients->reserve(100);
+    checkForClients();
 }
 
 MainWindow::~MainWindow()
@@ -39,17 +53,33 @@ void MainWindow::recordStop() {
 
 void MainWindow::on_pushButton_2_clicked()
 {
-    QStandardItemModel *model = new QStandardItemModel(this);
-    QStandardItem *item1 = new QStandardItem("test");
-    QStandardItem *item2 = new QStandardItem("test2");
-    model->appendRow(item1);
-    model->appendRow(item2);
+    //Ui::TClient *test1, *test2;
+    //test1 = new Ui::TClient{"Client 1", "asdf"};
 
-    QTableWidget *tview = ui->tableWidget->setModel(model);
+    QMessageBox qmsg;
 
-    /*if (ui->listView->viewMode() == ListMode) {
-        ui->debugTextEdit->insertPlainText("ListView ist in ListMode");
-    } else {
-        ui->debugTextEdit->insertPlainText("ListView ist in IconMode");
-    }*/
+
+
+    //clients << test1;
+    //clients->insert(0, &test1);
+    //clients[0] = &test1;
+    //clients->insert(0,QString("Client 1"));
+    clients->insert(clients->size(),"Client 2");
+
+    qmsg.setText(QString::number(clients->size()));
+    qmsg.exec();
+    //clients << QString("Client1") << QString("Client2");
+
+    checkForClients();
+}
+
+void MainWindow::checkForClients() {
+    /* some magic to check for clients */
+
+    for (int i = 0; i < clients->size(); i++) {
+        ui->tableWidget->setItem(i, 0, new QTableWidgetItem(clients->at(i)));
+        ui->tableWidget->setItem(i, 1, new QTableWidgetItem(""));
+        ui->tableWidget->item(i,1)->setBackgroundColor(QColor("red"));
+    }
+
 }
