8af2e4a48c9c30422aecf6cabd792690084c5e5f
DecayMehls <ymschaedle@gmail.com>
2016-03-14 17:53:24 +0100

    Fix Client Start Connection Loop Active

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char *argv[]) {
   QCoreApplication a(argc, argv);
 
   Client client;
-//  while (client.getState() == QAbstractSocket::UnconnectedState) {
-   client.start("192.168.1.1");
- // }
+  while (client.getState() == QAbstractSocket::UnconnectedState) {
+    client.start("192.168.1.1");
+  }
   return a.exec();
 }
